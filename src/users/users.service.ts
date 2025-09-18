05fb9237f8438ec1ab5af210b79af331646a71b7
Lemonadeccc <zwjhb12@163.com>
2025-09-18 21:52:22 +0800

    init: init backend with nestjs.

---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaServce: PrismaService) {}
+  async createUser(data: CreateUserRequest): Promise<Omit<User, 'password'>> {
+    try {
+      return await this.prismaServce.user.create({
+        data: { ...data, password: await bcrypt.hash(data.password, 10) },
+        select: {
+          email: true,
+          id: true,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw error;
+    }
+  }
+}
