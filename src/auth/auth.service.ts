5ef5a3eba713fa112d6081c3c8313dae3bc76f92
zhangwenjin <zwjhb12@163.com>
2025-09-26 14:21:17 +0800

    feat: add websocket authentication.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -51,4 +51,8 @@ export class AuthService {
       throw new UnauthorizedException('Credentials is not valid.');
     }
   }
+
+  verifyToken(jwt: string) {
+    this.jwtService.verify(jwt);
+  }
 }
--- src/products/products.gateway.ts
+++ src/products/products.gateway.ts
@@ -1,11 +1,26 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'http';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ProductsGateway {
+  constructor(private readonly authService: AuthService) {}
+
   @WebSocketServer()
   private readonly server: Server;
   handleProductUpdated() {
     this.server.emit('productUpdated');
+  }
+
+  handleConnection(client: Socket) {
+    try {
+      this.authService.verifyToken(client.handshake.auth.Authentication.value);
+    } catch (err) {
+      throw new WsException('Unauthorized.');
+    }
   }
 }
